3bde1cd3473273e1065f30a2de2a41c2393ef53a
saikat373 <saikatmohanty6@gmail.com>
2025-02-09 18:25:04 +0530

    login complete

--- src/components/users/Login/index.js
+++ src/components/users/Login/index.js
@@ -8,11 +8,11 @@ const Login = () => {
     return (
         <div className="login-container">
             <div className="login-form">
-                <h2>Login</h2>
+                <h2 className="login-title">Welcome to AI Universe</h2>
                 <form>
                     <input type="text" placeholder="Username" />
                     <input type="password" placeholder="Password" />
-                    <button>Login</button>
+                    <button>Enter</button>
                 </form>
             </div>
         </div>
